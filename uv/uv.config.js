453decf3a372f4d3a03e91f6ba61d771f6c597d3
boykoizmael <59s6w4tp7v@privaterelay.appleid.com>
2026-09-22 23:57:49 -0700

    Create uv.config.js

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/service/',
+    bare: '/bare/',
+    encodeUrl: function(url) { return encodeURIComponent(url); },
+    decodeUrl: function(url) { return decodeURIComponent(url); },
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js'
+};
