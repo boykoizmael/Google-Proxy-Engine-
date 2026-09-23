dd0e9c32ce68458e0ee9c0646f7d80f560f9de75
boykoizmael <59s6w4tp7v@privaterelay.appleid.com>
2026-09-22 23:47:50 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,6 +6,7 @@ importScripts('https://jsdelivr.net');
 const uv = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
+    // If the path namespace matches the prefix target block, let the worker resolve assets locally
     if (event.request.url.startsWith(self.location.origin + __uv$config.prefix)) {
         event.respondWith(uv.fetch(event));
     }
