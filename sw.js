eada8e62e74afadc4c3f67adebeeef726e735ba9
boykoizmael <59s6w4tp7v@privaterelay.appleid.com>
2026-09-22 23:59:20 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,10 @@
-// sw.js - Clean Script Router Mapping
-importScripts('/uv.bundle.js');
-importScripts('/uv.config.js');
-importScripts('https://jsdelivr.net');
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts('/uv/uv.sw.js');
 
 const uv = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
-    // If the path namespace matches the prefix target block, let the worker resolve assets locally
     if (event.request.url.startsWith(self.location.origin + __uv$config.prefix)) {
         event.respondWith(uv.fetch(event));
     }
